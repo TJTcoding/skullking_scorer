b1e25fd11b3c79862624c069494a0fac97dadc68
Jon Timothy <259536807+jonTimothy@users.noreply.github.com>
2026-02-05 14:12:04 +0200

    update service worker

--- assets/js/script.js
+++ assets/js/script.js
@@ -380,3 +380,9 @@ function restart() {
         location.reload();
     }
 }
+
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker.register("/service-worker.js")
+    .then(() => console.log("Service Worker registered"))
+    .catch(err => console.error("SW registration failed", err));
+}
